8090502d99df9073149cc662feb4631301c4c2f1
leo.daunbi <leothai.daunbi@gmail.com>
2023-03-21 16:52:05 +0900

    ss

--- laravel/app/Http/Controllers/PostController.php
+++ laravel/app/Http/Controllers/PostController.php
@@ -18,9 +18,33 @@ class PostController extends Controller
 
 
     public function getAllPosts(){
-        $posts = Post::with('post_images') ->orderBy('crated_at', 'desc')->get();
+        $posts = Post::with('post_images')->orderBy('created_at', 'desc')->get();
         return response()->json(['error'=>false, 'data' => $posts]);
     }
+    
+    public function createPost(Request $request){
+        $user = Auth::user();
+        $title = $request->title;
+        $body = $request->body;
+        $images = $request->images;
+    
+        $post = Post::create([
+            'title' => $title,
+            'body' => $body,
+            'user_id' => $user->id,
+        ]);
+    
+        foreach($images as $image){
+            $imagePath = Storage::disk('uploads')->put($user->email . '/posts',$image);
+            PostImage::create([
+                'post_image_caption' =>  $title,
+                'post_image_path' => '/uploads' .$imagePath,
+                'post_id' => $post->id
+            ]);
+        }
+        return response()->json(['error' => false, 'data' => $post]);
+    }
+    
     /**
      * Store a newly created resource in storage.
      *
--- laravel/resources/views/home.blade.php
+++ laravel/resources/views/home.blade.php
@@ -1,5 +1,3 @@
-@extends('layouts.app')
-
 @section('content')
 <div class="clearfix">
     <h2 class="float-left">List of all projects</h2>
@@ -8,36 +6,44 @@
     <a href="{{ route('posts.create') }}" class="btn btn-link float-right">Create new post</a>
 </div>
 
-{{-- List all posts --}}
-@forelse ($posts as $post)
-    <div class="card m-2 shadow-sm">
-        <div class="card-body">
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <create-post />
+        </div>
+        <div class="col-md-6 posts-container" style="height:35rem; overflow-y: scroll">
+            {{-- List all posts --}}
+            @forelse ($posts as $post)
+                <div class="card m-2 shadow-sm">
+                    <div class="card-body">
 
-            {{-- post title --}}
-            <h4 class="card-title">
-                <a href="{{ route('posts.show', $post->id) }}">{{ $post->title }}</a>
-            </h4>
+                        {{-- post title --}}
+                        <h4 class="card-title">
+                            <a href="{{ route('posts.show', $post->id) }}">{{ $post->title }}</a>
+                        </h4>
 
-            <p class="card-text">
-                
-                {{-- post owner --}}
-                <small class="float-left">By: {{ $post->owner->name }}</small>
+                        <p class="card-text">
+                            
+                            {{-- post owner --}}
+                            <small class="float-left">By: {{ $post->owner->name }}</small>
 
-                {{-- creation time --}}
-                <small class="float-right text-muted">{{ $post->created_at->format('M d, Y h:i A') }}</small>
-                
-                {{-- check if the signed-in user is the post owner, then show edit post link --}}
-                @if (auth()->id() == $post->owner->id )
-                    {{-- edit post link --}}
-                    <small class="float-right mr-2 ml-2">
-                        <a href="{{ route('posts.edit', $post->id) }}" class="float-right">edit your post</a>
-                    </small>
-                @endif
-            </p>
+                            {{-- creation time --}}
+                            <small class="float-right text-muted">{{ $post->created_at->format('M d, Y h:i A') }}</small>
+                            
+                            {{-- check if the signed-in user is the post owner, then show edit post link --}}
+                            @if (auth()->id() == $post->owner->id )
+                                {{-- edit post link --}}
+                                <small class="float-right mr-2 ml-2">
+                                    <a href="{{ route('posts.edit', $post->id) }}" class="float-right">edit your post</a>
+                                </small>
+                            @endif
+                        </p>
+                    </div>
+                </div>
+            @empty
+                <p>No posts yet, stay tuned!</p>
+            @endforelse
         </div>
     </div>
-@empty
-    <p>No posts yet, stay tuned!</p>
-@endforelse
-
+</div>
 @endsection
